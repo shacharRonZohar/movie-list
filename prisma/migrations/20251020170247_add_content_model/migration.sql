a38f1313f7136c6d7905ba2db426b39ee0855654
Shachar Ron Zohar <shachar.ron@lilibanking.com>
2025-10-20 20:03:18 +0300

    Add Content model and Status enum to Prisma schema
    - Introduced a new enum 'Status' with values for content tracking.
    - Added 'Content' model with fields for title, description, status, and relationships to the User model.
    - Created migration SQL for the new Content table and associated foreign keys.

---
+++ prisma/migrations/20251020170247_add_content_model/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('WANT_TO_WATCH', 'WATCHING', 'WATCHED', 'ON_HOLD', 'DROPPED');
+
+-- CreateTable
+CREATE TABLE "content" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'WANT_TO_WATCH',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by_id" TEXT NOT NULL,
+    "requested_by_id" TEXT NOT NULL,
+    "order" SERIAL NOT NULL,
+
+    CONSTRAINT "content_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "content" ADD CONSTRAINT "content_created_by_id_fkey" FOREIGN KEY ("created_by_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "content" ADD CONSTRAINT "content_requested_by_id_fkey" FOREIGN KEY ("requested_by_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
